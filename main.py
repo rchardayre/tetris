0ed5ae43beb68a0e3bde66945a2cd031161ea85e
Romain Chardayre <chardayre.romain@gmail.com>
2018-05-14 10:52:48 +0200

    add run_tetris func

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import sys
 import time
 import argparse
 
-tetris_grid = []
-record = {'moves' : [], 'forms' : []}
-replay_idx = 0
 
 def draw():
     global window
@@ -21,8 +18,8 @@ def draw():
 
 def update(delta_t):
     if config.is_replay:
-        global replay_idx
-        for move in record['moves'][replay_idx]:
+        global playback_idx
+        for move in recordData['moves'][playback_idx]:
             #time.sleep(config.update_frequency / 10)
             if move == grid.MOVE_LEFT:
                 tetris_grid.move_left()
@@ -37,46 +34,42 @@ def update(delta_t):
             elif move == grid.HARD_DROP:
                 tetris_grid.hard_drop()
 
-        replay_idx = replay_idx + 1
+        playback_idx = playback_idx + 1
     if tetris_grid.clock_update() == grid.GAME_OVER:
         if config.is_record:
             with open("./record.json", 'w') as f:
-                json.dump(record, f)
+                json.dump(recordData, f)
             
         print("GAME OVER")
         print("FINAL SCORE : %d" % tetris_grid.score)
         exit(0)
 
-
-if __name__ == "__main__":
-    print("Launching Tetris...")
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--simulation', dest='simulation', action='store_true') 
-    parser.add_argument('--playback', dest='playback_file', type=str, default ='')
-    parser.add_argument('--record', dest='record', action='store_true')
-    parser.add_argument('--freq', dest='frequency', type=float, default=0.2)
+ 
+def run_tetris(is_record, is_simulation, playback_file, frequency):
+    global recordData
     
-    options = parser.parse_args()
-    
-    if len(sys.argv) > 1:
-        filename = options.playback_file
-        if filename != '':
-            if options.record:
-                print("Cannot record a playback")
-                options.record = False
-            try:
-                with open(filename) as f:
-                    record = json.load(f)
-                    config.is_replay = True
-            except Exception:
-                print("Invalid playback file")
-                sys.exit(1)
+    recordData = {'moves' : [], 'forms' : []}
+     
+    if playback_file != '':
+        if is_record:
+            print("Cannot record a playback")
+            is_record = False
+        try:
+            with open(playback_file) as f:
+                recordData = json.load(f)
+                config.is_replay = True
+                global playback_idx
+                playback_idx = 0
+        except Exception:
+            print("Invalid playback file")
+            sys.exit(1)
 
-    config.is_record = options.record
-    config.is_simulation = options.simulation
-    config.update_frequency = options.frequency
+    config.is_record = is_record
+    config.is_simulation = is_simulation
+    config.update_frequency = frequency
 
-    tetris_grid = grid.Grid(config.nb_block_horizontal, config.nb_block_vertical, record)
+    global tetris_grid
+    tetris_grid = grid.Grid(config.nb_block_horizontal, config.nb_block_vertical, recordData)
     if config.is_simulation:
         while(True):
             update(0)
@@ -97,6 +90,16 @@ if __name__ == "__main__":
     pyglet.app.run()
 
     
-    
+if __name__ == "__main__":
+    print("Launching Tetris...")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--simulation', dest='simulation', action='store_true')
+    parser.add_argument('--playback', dest='playback_file', type=str, default ='')
+    parser.add_argument('--record', dest='record', action='store_true')
+    parser.add_argument('--freq', dest='frequency', type=float, default=0.2)
+
+    options = parser.parse_args()
+
+    run_tetris(options.record, options.simulation, options.playback_file, options.frequency)    
 
     
